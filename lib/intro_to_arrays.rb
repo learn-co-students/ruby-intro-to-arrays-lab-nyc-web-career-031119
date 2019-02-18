d166dc3229a47798890ad6ddc4ce32ab0b0b8f59
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-18 22:35:44 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,33 @@
+def instantiate_new_array
+  my_new_array = []
+end
+
+def array_with_two_elements
+  my_new_array = [2,4]
+end
+
+def first_element(arr)
+  return arr[0]
+end
+
+def last_element(arr)
+  return arr[-1]
+end
+
+def third_element(arr)
+  return arr[2]
+end
+
+def first_element_with_array_methods(arr)
+  temp = arr.first
+  return temp
+end
+
+def last_element_with_array_methods(arr)
+  temp = arr.last
+  return temp
+end
+
+def length_of_array(arr)
+  return arr.length
+end
