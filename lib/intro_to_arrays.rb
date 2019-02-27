aced7c0885459b81273c47c489bd860e8469a1dd
github username <github email address>
2019-02-27 12:23:31 -0500

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,32 @@
+def instantiate_new_array
+  arr = []
+  arr
+end
+
+def array_with_two_elements
+  [rand(1..9),rand(1..9)]
+end
+
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[array.length-1]
+end
+
+def first_element_with_array_methods(array)
+  first_element(array)
+end
+
+def last_element_with_array_methods(array)
+  last_element(array)
+end
+
+def length_of_array(array)
+  array.length
+end
